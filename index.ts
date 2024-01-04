54a875447d3aa286248f7e6194d3453bdccc6ae0
TF Mukles <tfmukles@Mukles-2.local>
2024-01-04 15:55:22 +0600

    sum

--- index.ts
+++ index.ts
@@ -1,8 +1,4 @@
 function myFunction() {
   const a = [1, 2, 3, 4];
-  // const a = [1, 2, 3, 4];
-  // const a = [1, 2, 3, 4];
-  // const a = [1, 2, 3, 4];
-  // const a = [1, 2, 3, 4];
-  // const a = [1, 2, 3, 4];
+  let sum = a.reduce((a, b) => a + b, 0);
 }
