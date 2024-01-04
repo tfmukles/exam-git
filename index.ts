e9d014b4aaa0ea55fc121a6dc202f021cbf4b002
TF Mukles <tfmukles@Mukles-2.local>
2024-01-04 15:52:34 +0600

    refactor my fucntion

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 function myFunction() {
   const a = [1, 2, 3, 4];
+  // const a = [1, 2, 3, 4];
+  // const a = [1, 2, 3, 4];
+  // const a = [1, 2, 3, 4];
+  // const a = [1, 2, 3, 4];
+  // const a = [1, 2, 3, 4];
 }
